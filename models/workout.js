540cb3bfa43ab31ce8f82faa00f354c7f238a767
Seychelle Koplik <sey.koplik@gmail.com>
2020-10-21 14:53:17 -0400

    designed WorkoutSchema

--- models/workout.js
+++ models/workout.js
@@ -5,8 +5,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    day: Date,
-    exercises: Array
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Must enter a type of exercise!"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Must enter an execise name!"
+            },
+            duration: {
+                type: Number,
+                required: "Must enter how long you exercised!"
+            },
+            weight: { type: Number },
+            reps: { type: Number },
+            sets: { type: Number },
+            distance: { type: Number },
+        }
+    ]
 }, { timestamps: true });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
